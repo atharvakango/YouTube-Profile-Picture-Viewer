b06fc2226253382628643c84d79a83b3e6faf0ca
atharvakango <atharvakango1@gmail.com>
2020-10-10 18:25:38 +0530

    A raw but, working version completed. Modal window to be added in the future commits

--- content.js
+++ content.js
@@ -1,20 +1,37 @@
 console.log("Chrome Extension ready to Go!");
 
-var x = document.getElementsByClassName("style-scope yt-img-shadow");
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
+// var iframe  = document.createElement ('div');
+// iframe.src  = chrome.extension.getURL ('template.html');
+// document.body.appendChild (iframe);
+// //-----
+// $.get(chrome.runtime.getURL('/template.html'), function(data) {
+  
+//     $(data).appendTo('body');
+  
+// });
+//------
+
+
+var x = document.getElementsByClassName("style-scope ytd-c4-tabbed-header-renderer no-transition");
+// var modal = document.getElementById("myModal");
+// var modalImg = document.getElementById("img01");
 x[0].style.cursor = "pointer";
 
 x[0].onclick = function() {
-	var t = x[0].getAttribute("src");
-    modal.style.display = "block";
-    modalImg.src = t.replace("=s1", "=s10");
+	// ;
+	var t = x[0].children[0].getAttribute("src");
+console.log(t);
+    // modal.style.display = "block";
+    // modalImg.src = t.replace("=s1", "=s10");
+    var newLink = t.replace("=s1", "=s10");
+    console.log(newLink);
+    window.location = newLink;
 }
 
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() { 
-    modal.style.display = "none";
-}
+// var span = document.getElementsByClassName("close")[0];
+// span.onclick = function() { 
+//     modal.style.display = "none";
+// }
 
 console.log("Chrome Extension Ended Execution!");
 
